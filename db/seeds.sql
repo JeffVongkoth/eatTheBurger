56e12a85639436bb1ce8f1c62f698b683ee7d6c3
Jeff Vongkoth <jvongkoth@gmail.com>
2018-07-18 15:18:39 -0500

    most recent version

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Big Mac');
+INSERT INTO burgers (burger_name) VALUES ('Whopper');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Baconator', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double cheese burger w/ Bacon', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Heart Attack Special', true);
+INSERT INTO burgers (burger_name) VALUES ('Chipotle Burger');
---
+++ server.js
@@ -0,0 +1,36 @@
+// DEPENDENCIES
+
+var express = require("express");
+var bodyParser = require("body-parser");
+
+// Set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var PORT = process.env.PORT || 3000;
+
+// Create an instance of the express app.
+var app = express();
+
+app.use(express.static("public"));
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+var exphbs = require("express-handlebars");
+
+// Set Handlebars as the default templating engine.
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+var routes = require("./controllers/burgers_controllers.js");
+
+app.use(routes);
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function() {
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
+  });
